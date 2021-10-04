fca26634efe78a3022092aca432abc96ed4c8214
celebi law <celebilaw@gmail.com>
2021-10-04 01:15:15 -0700

    95% finished
    Just need to spawn them all randomly

---
+++ ACM Studio Team 6 LD49/Assets/Scripts/enemyAI/enemyFly.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyFly : MonoBehaviour
+{
+    private float speed;
+    [SerializeField] Vector3[] positions;
+    [SerializeField] Vector3 startPos;
+    [SerializeField] float attackTimer;
+    public float countDown;
+    private bool startTimer;
+    private bool attack;
+    private bool idle;
+    private int index;
+
+    public Transform player;
+    // temporary position for enemy attack
+    private Vector3 pos;
+
+    void Start()
+    {
+        countDown = attackTimer;
+        startTimer = true;
+        attack = false;
+        idle = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (idle)
+        {
+            speed = 1;
+            Fly();
+        }
+
+        if (startTimer == true)
+        {
+            countDown -= Time.deltaTime;
+        }
+
+        if (countDown <= 0)
+        {
+            pos = player.position;
+            countDown = attackTimer;
+            speed = 10;
+            startTimer = false;
+            attack = true;
+            idle = false;
+        }
+
+        if (!startTimer && attack)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        }
+
+        if (!startTimer && !attack)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, startPos, Time.deltaTime * speed);
+        }
+
+        if (transform.position == startPos && !startTimer)
+        {
+            startTimer = true;
+            idle = true;
+        }
+
+        if (transform.position == pos && attack)
+        {
+            attack = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            // add damage to player
+            attack = false;
+        }
+    }
+
+    private void Fly()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, positions[index], Time.deltaTime * speed);
+        if (transform.position == positions[index])
+        {
+            if (index == positions.Length - 1)
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+        }
+    }
+}
